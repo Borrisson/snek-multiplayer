83d753a526a1b49ab724432761c3c0c1dba0c830
Borrisson <lucborris@gmail.com>
2021-01-13 18:25:39 +0000

    added canned messages to the file

--- snake-client/input.js
+++ snake-client/input.js
@@ -1,33 +1,40 @@
 let connection;
-const setupInput = function (conn) {
+const setupInput = function(conn) {
   connection = conn;
   const stdin = process.stdin;
   stdin.on('data', (data) => {
     handleUserInput(data);
-  })
+  });
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   return stdin;
-}
+};
 setupInput();
 
-const handleUserInput = function (key) {
+const handleUserInput = function(key) {
   switch (key) {
-    case '\u0003':
-      process.exit();
-      break;
-    case 'w':
-      connection.write('Move: up');
-      break;
-    case 'd':
-      connection.write('Move: down');
-      break;
-    case 'a':
-      connection.write('Move: left');
-      break;
-    case 's':
-      connection.write('Move: right');
+  case '\u0003':
+    process.exit();
+    break;
+  case 'w':
+    connection.write('Move: up');
+    break;
+  case 'd':
+    connection.write('Move: down');
+    break;
+  case 'a':
+    connection.write('Move: left');
+    break;
+  case 's':
+    connection.write('Move: right');
+    break;
+  case 'y':
+    connection.write("Say: They see me Rollin' ");
+    break;
+  case 'u':
+    connection.write("Say: They hatin'            ");
+    break;
   }
 };
 
