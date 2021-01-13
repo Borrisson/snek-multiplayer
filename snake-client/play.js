8e15e3793b3a418295742cb42393467cf99457db
Borrisson <lucborris@gmail.com>
2021-01-13 17:46:34 +0000

    added the ctrl + c exit command to play.js

--- snake-client/play.js
+++ snake-client/play.js
@@ -1,3 +1,21 @@
 const { connect } = require('./client');
 console.log('Connecting ...');
 connect();
+
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.on('data', (data) => {
+    handleUserInput(data);
+  })
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  return stdin;
+}
+setupInput();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
