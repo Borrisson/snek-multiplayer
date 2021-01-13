4755545b9c4b791e0cfce1437df57f18445c8ce0
Borrisson <lucborris@gmail.com>
2021-01-13 17:34:41 +0000

    tests with move commands on write

--- snake-client/client.js
+++ snake-client/client.js
@@ -4,12 +4,13 @@ const net = require('net');
 const connect = function () {
   const conn = net.createConnection({
     host: 'localhost',
-    port: 50541
+    port: 50541,
   });
   conn.on('connect', () => {
     console.log('Connection Established');
     conn.write(`Name: ${name}`);
-  })
+
+  });
   // interpret incoming data as text
   conn.setEncoding('utf8');
   conn.setTimeout(15000);
