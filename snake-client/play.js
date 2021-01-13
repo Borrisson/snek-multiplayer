b03b062f37be549ea8ab037cf53d80b915784913
Borrisson <lucborris@gmail.com>
2021-01-13 17:51:48 +0000

    refactored the setupInput and handleUserInput functions into a module file

--- snake-client/play.js
+++ snake-client/play.js
@@ -1,21 +1,8 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
+
+
 console.log('Connecting ...');
 connect();
 
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.on('data', (data) => {
-    handleUserInput(data);
-  })
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-  return stdin;
-}
 setupInput();
-
-const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-};
